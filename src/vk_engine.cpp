355998c76415e6ab0021dd423440993bc63aa975
iaomw <iaomw@live.com>
2022-10-11 23:24:10 +0800

    Typo

--- src/vk_engine.cpp
+++ src/vk_engine.cpp
@@ -370,7 +370,7 @@ void VulkanEngine::init_commands()
 void VulkanEngine::init_default_renderpass()
 {
 	VkAttachmentDescription color_attachment = {};
-	color_attachment.format = _swachainImageFormat;
+	color_attachment.format = _swapchainImageFormat;
 	color_attachment.samples = VK_SAMPLE_COUNT_1_BIT;
 	color_attachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
 	color_attachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
