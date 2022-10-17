1e86416c2cb800edb493d2a4ef9748cb17701350
iaomw <iaomw@live.com>
2022-10-17 20:26:11 +0800

    Build improve

--- build.sh
+++ build.sh
@@ -2,8 +2,10 @@ export CMAKE_TOOLCHAIN_FILE=${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake
 
 echo $CMAKE_TOOLCHAIN_FILE
 
-cmake -B build -DCMAKE_EXPORT_COMPILE_COMMANDS=1
+cmake -B build #-DCMAKE_EXPORT_COMPILE_COMMANDS=1
 
 export MAKEFLAGS=-j$(nproc)
 
-cmake --build build -j ${MAKEFLAGS}
+cmake --build build ${MAKEFLAGS}
+
+ln -s ./build/compile_commands.json ./
--- clean.sh
+++ clean.sh
@@ -1,2 +1,3 @@
-rm -rf build
 rm shaders/*.spv
+rm -rf build
+rm *.json
